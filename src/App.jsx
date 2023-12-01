d948f49117db91ce6e2ae94dd203d892ad22ca40
Yashwanth Reddy <yashwanthreddy072@gmail.com>
2023-12-01 18:15:10 +0530

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
+import CategoryPills from './components/CategoryPills'
+import categories from "./data/home"
+
 const App = () => {
+  const [selectedCategory,setSelectedCategory] = useState(categories[0])
   return (
     <div className=' max-h-screen flex flex-col'>
       <Header/>
-      <div>2</div>
+      <div className='grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto' >
+            <div>sidebar</div>
+            <div className='overflow-x-hidden px-8 pb-4'>
+            <div className='sticky top-0 bg-white z-10 pb-4'> 
+            <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory}/>
+            </div>
+            </div>
       </div>
+    </div>
   )
 }
 
---
+++ src/data/home.ts
@@ -0,0 +1,18 @@
+ const categories = [
+  "All",
+  "Live",
+  "Music",
+  "Web Devlopment",
+  "Gaming",
+  "5G",
+  "ReactJs",
+  "Entertinement",
+  "Movies",
+  "Anime",
+  "Sports",
+  "lifeStyle",
+  "Vlogs",
+  "Technology",
+  "programming",
+];
+export default categories
